a13e8336b69fd4c7346ec57cad101ab0d2da5fa6
정호진 <dnjfs@naver.com>
2024-03-14 20:05:08 +0900

    TopDown 템플릿 업로드
    - 추후 StarterContent 정리 필요

---
+++ Source/SnowWar/SnowWar.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SnowWar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnowWar, "SnowWar" );
+
+DEFINE_LOG_CATEGORY(LogSnowWar)
+ 
---
+++ Source/SnowWar/SnowWarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SnowWarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASnowWarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASnowWarGameMode();
+};
+
+
+
